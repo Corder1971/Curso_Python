37802a83bb5fd7f2c0ea74407f4ae0670df24281
¨Corder1971¨ <donatemiguel@gmail.com>
2025-08-07 19:02:38 +0200

    V0.0.3

---
+++ eje2.py
@@ -0,0 +1,8 @@
+str = 'geeks de pruebea de práctica de código'
+def rev_sentence(sentence):
+    words = sentence.split('')
+    reverse_sentence = ' '.join(reversed(words))
+    return reverse_sentence
+if __name__ == "_main_":
+    input = 'geeks quiz practice code'
+    print (rev_sentence(input))
