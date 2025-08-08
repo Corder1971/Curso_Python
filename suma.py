01d5677349b4aae7b363f1ed048f014dd8cda75f
¨Corder1971¨ <donatemiguel@gmail.com>
2025-08-08 18:39:24 +0200

    V0.0.3

--- suma.py
+++ suma.py
@@ -1,6 +1,8 @@
 def suma(a, b): # Definimos la función suma
     return a + b # "Ruturn" suma de a y b 
-x= suma(2, 3) # Guardamos el resultadado en x
+# x= suma(2, 3) # Función con ints
+x= suma(5.7, 4.0) # Función con floats 
+# x= suma('Me gusta', 'python') # Función con strings
 print(x)
 
 
